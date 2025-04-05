35e3dc85136d23d0e945dfc23714df8c8540e50a
Orasan Adrian Ionel <orasanadrian1@gmail.com>
2025-04-05 17:48:34 +0300

    catalog

--- mysite/itec/urls.py
+++ mysite/itec/urls.py
@@ -5,4 +5,9 @@ from django.urls import path
 urlpatterns = [
     path("",views.home, name="home"),
     path("manele",views.manele, name="manele"),
+    path("rap",views.rap, name="rap"),
+    path("trap",views.trap, name="trap"),
+    path("rock",views.rock, name="rock"),
+    path("pop",views.pop, name="pop"),
+    path("jaz",views.jaz, name="jaz"),
 ]
--- mysite/itec/views.py
+++ mysite/itec/views.py
@@ -9,4 +9,19 @@ def manele(request):
     context={}
     return render(request, "itec/manele.html", context)
 
-# views.py
+def rock(request):
+    context={}
+    return render(request, "itec/rock.html", context)
+def rap(request):
+    context={}
+    return render(request, "itec/rap.html", context)
+def trap(request):
+    context={}
+    return render(request, "itec/trap.html", context)
+
+def jaz(request):
+    context={}
+    return render(request, "itec/jaz.html", context)
+def pop(request):
+    context={}
+    return render(request, "itec/pop.html", context)
