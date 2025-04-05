9e1634960285afc57615a94ac11c4b8f26dc2ae4
Orasan Adrian Ionel <orasanadrian1@gmail.com>
2025-04-05 13:37:38 +0300

    manale

--- mysite/itec/urls.py
+++ mysite/itec/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("",views.home, name="home")
+    path("",views.home, name="home"),
+    path("manele",views.manele, name="manele")
 ]
--- mysite/itec/views.py
+++ mysite/itec/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     context={}
     return render(request, "itec/home.html", context)
+
+def manele(request):
+    context={}
+    return render(request, "itec/manele.html", context)
